e9db2525da7e8308ad65ee59c10ee04e5578fc55
tacobayle <nicolas.bayle@gmail.com>
2021-07-22 17:48:44 +0000

    20210722

--- day2/04_app/app.tf
+++ day2/04_app/app.tf
@@ -23,7 +23,7 @@ resource "vsphere_virtual_machine" "backend_vmw" {
   name             = "backend_vmw-${count.index}"
   datastore_id     = data.vsphere_datastore.datastore.id
   resource_pool_id = data.vsphere_resource_pool.pool.id
-  folder           = vsphere_folder.folder.path
+  folder           = data.vsphere_folder.folder.path
 
   network_interface {
     network_id = data.vsphere_network.networkMgt.id
